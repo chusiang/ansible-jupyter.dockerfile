5dcf5ce9ab745a6d00331bdf90131b5aa6bb4346
Jonny Lai <jonny.lai@commandp.com>
2016-11-23 11:40:18 +0800

    add Dockerfile for CentOS 6

---
+++ centos-6/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# ============================================================
+#  Author: Chu-Siang Lai / chusiang (at) drx.tw
+#  Blog: http://note.drx.tw
+#  Filename: docker-entrypoint.sh
+#  Modified: 2016-11-20 18:43
+#  Description: Run the jupyter service.
+#
+#   --ip 0.0.0.0: Allow all IP access.
+#   --no-browser: Don't open browser from command line.
+#   --notebook-dir: Bunding the workdir.
+#
+# =========================================================== 
+
+#jupyter-notebook --ip 0.0.0.0 --no-browser --notebook-dir=/home
+/opt/anaconda2/bin/jupyter-notebook --ip 0.0.0.0 --no-browser --notebook-dir=/home

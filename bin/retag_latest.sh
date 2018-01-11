5cb1169a9b2afac0a1843536460c7d90e7fe54ed
Chu-Siang Lai <chusiang.lai@gmail.com>
2018-01-11 10:37:36 +0800

    Reuse the docker image name variable

--- bin/retag_latest.sh
+++ bin/retag_latest.sh
@@ -3,17 +3,17 @@
 DOCKER_IMAGE="chusiang/ansible-jupyter"
 DOCKER_TAG="alpine-3.6"
 
-echo 'Pull alpine image.'
+echo '===> Pull current image ...'
 docker pull $DOCKER_IMAGE:$DOCKER_TAG
 echo
 
-echo 'Tag alpine image to latest.'
+echo '===> Tag current image to latest ...'
 docker tag $DOCKER_IMAGE:$DOCKER_TAG $DOCKER_IMAGE:latest
 echo
 
-echo 'Push the latest tag.'
+echo '===> Push the latest tag ...'
 docker push $DOCKER_IMAGE:latest
 echo
 
-echo 'Remove old image.'
-docker rmi $(docker images | grep 'chusiang/ansible-jupyter' | grep '<none>' | awk '{ print $3 }')
+echo '===> Remove old image ...'
+docker rmi $(docker images | grep $DOCKER_IMAGE | grep '<none>' | awk '{ print $3 }')
